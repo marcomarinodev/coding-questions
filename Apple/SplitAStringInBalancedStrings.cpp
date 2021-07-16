75c598fe5f013a8dc10c2bd8d82eaa8195be74f3
Marco Marinò <marco.marin12@outlook.it>
2021-07-16 17:24:42 +0200

    Split a String In Balanced Strings
    Apple Coding Interview question #1221

---
+++ Apple/SplitAStringInBalancedStrings.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  Split a String in Balanced Strings
+//
+//  Created by Marco Marinò on 16/07/21.
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	 int balancedStringSplit(string s) {
+		 int counter = 0;
+		 int l_sequence = 0;
+		 int r_sequence = 0;
+
+		 for (string::size_type i = 0; i < s.size(); i++) {
+			 
+			 if (s[i] == 'L') {
+				 l_sequence++;
+			 } else {
+				 r_sequence++;
+			 }
+			 
+			 if (l_sequence == r_sequence) {
+				 counter++;
+				 l_sequence = 0;
+				 r_sequence = 0;
+			 }
+		 }
+		 
+		 return counter;
+	 }
+	
+	void test() {
+		int test1 = balancedStringSplit("RLRRLLRLRL");
+		int test2 = balancedStringSplit("RLLLLRRRLR");
+		int test3 = balancedStringSplit("LLLLRRRR");
+		int test4 = balancedStringSplit("RLRRRLLRLL");
+		
+		assert(test1 == 4);
+		assert(test2 == 3);
+		assert(test3 == 1);
+		assert(test4 == 2);
+		
+		cout << "Test passed!" << endl;
+	}
+};
+
+int main(int argc, const char * argv[]) {
+	
+	Solution * sol = new Solution();
+	
+	sol->test();
+	
+	return 0;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetcode-questions
-Collection of solved coding interview problems on leetcode.com
+Collection of solved coding interview problems on leetcode.com. 
+You can find the url of the problem inside the solution file.
+Each folder represents the company where the questions inside that folder came from.
